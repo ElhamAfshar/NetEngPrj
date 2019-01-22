096393994721006f8f4656d8565289c20fc323f4
CleVer <elhamafshar2100@gmail.com>
2019-01-22 15:25:15 +0330

    Add project files.

---
+++ NetEngPrj/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NetEngPrj.MvcApplication" Language="C#" %>
---
+++ NetEngPrj/Models/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NetEngPrj.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string  firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Ssn { get; set; }
+        public string Code { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+        public bool Married { get; set; }
+        public int Age { get; set; }
+        public double Salary { get; set; }
+
+    }
+}
